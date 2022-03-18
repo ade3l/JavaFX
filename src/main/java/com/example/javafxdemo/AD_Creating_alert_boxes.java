8947beb5e1f7a6e57e5526a22106d5b0b56ffde9
Adeel <71775487+ade3l@users.noreply.github.com>
2022-03-18 10:02:09 +0530

    Added code for showing an alert box

--- src/main/java/com/example/javafxdemo/AD_Creating_alert_boxes.java
+++ src/main/java/com/example/javafxdemo/AD_Creating_alert_boxes.java
@@ -1,9 +1,13 @@
 package com.example.javafxdemo;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class AD_Creating_alert_boxes extends Application {
@@ -13,12 +17,32 @@ public class AD_Creating_alert_boxes extends Application {
         launch(args);
     }
 
+//    Put this in a class if you want to make alert box objects;
+    public static void display(String title, String message){
+        Stage window = new Stage();
+//        Set the modality so that while this window is open you cannot interact with the other one
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
 
+        Label label = new Label(message);
+        Button button = new Button("Close the window");
+        button.setOnAction(actionEvent -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label,button);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
         window=stage;
         button=new Button("Click me");
+        button.setOnAction(e -> display("Title of window","This is the alert window"));
         StackPane layout= new StackPane();
         layout.getChildren().add(button);
         Scene scene = new Scene(layout,300,300);
