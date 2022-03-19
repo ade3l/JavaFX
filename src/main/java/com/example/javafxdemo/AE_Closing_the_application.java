2c2009e1bf8def3abd2466bc61e8f94e03b41f6b
Adeel <71775487+ade3l@users.noreply.github.com>
2022-03-20 01:17:10 +0530

    Code to handle application close request added

--- src/main/java/com/example/javafxdemo/AE_Closing_the_application.java
+++ src/main/java/com/example/javafxdemo/AE_Closing_the_application.java
@@ -1,10 +1,12 @@
 package com.example.javafxdemo;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class AE_Closing_the_application extends Application {
     Stage window;
@@ -16,12 +18,24 @@ public class AE_Closing_the_application extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         window = stage;
+        window.setTitle("Main window");
+        window.setOnCloseRequest(e-> {
+            e.consume();
+            closeProgram();
+        });
         button = new Button("Close Program");
-        button.setOnAction(e -> window.close());
+        button.setOnAction(e -> closeProgram());
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
         Scene scene = new Scene(layout, 200, 200);
         window.setScene(scene);
         window.show();
+    }
+
+    private void closeProgram() {
+        System.out.println("Called");
+        Boolean answer= AD3_Confirm_Box.display("Title", "Are you sure you want to quit");
+        if(answer)
+            window.close();
     }
 }
