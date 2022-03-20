6a03dde55592f84470595be7336ba699fb2fb72c
Adeel <71775487+ade3l@users.noreply.github.com>
2022-03-20 15:35:03 +0530

    border pane code added

--- src/main/java/com/example/javafxdemo/AF_Embedding_layouts.java
+++ src/main/java/com/example/javafxdemo/AF_Embedding_layouts.java
@@ -2,7 +2,11 @@ package com.example.javafxdemo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AF_Embedding_layouts extends Application {
@@ -16,8 +20,23 @@ public class AF_Embedding_layouts extends Application {
         window= stage;
         window.setTitle("Title of window");
 
-        StackPane layout = new StackPane();
-        Scene scene = new Scene(layout, 400, 500);
+        HBox topMenu = new HBox();
+        Button buttonA = new Button("File");
+        Button buttonB = new Button("Edit");
+        Button buttonC = new Button("View");
+        topMenu.getChildren().addAll(buttonA,buttonB,buttonC);
+
+        VBox leftMenu = new VBox();
+        Button buttonD = new Button("D");
+        Button buttonE = new Button("E");
+        Button buttonF = new Button("F");
+        leftMenu.getChildren().addAll(buttonD,buttonE,buttonF);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
+
+        Scene scene = new Scene(borderPane, 400, 500);
         window.setScene(scene);
         window.show();
 
