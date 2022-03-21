9197bbc1e40f2e4f30fd24fa511f9896704b645e
Adeel <71775487+ade3l@users.noreply.github.com>
2022-03-21 23:58:23 +0530

    Get data from text field and validate code added

--- src/main/java/com/example/javafxdemo/AH_Extract_Validate_input.java
+++ src/main/java/com/example/javafxdemo/AH_Extract_Validate_input.java
@@ -16,15 +16,28 @@ public class AH_Extract_Validate_input extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         window = stage;
-        TextField nameIp = new TextField();
-        Button button = new Button("Click me");
-
+        TextField ageIp = new TextField();
+        Button button = new Button("Validate");
+        button.setOnAction(e -> {
+            if(isInt(ageIp,ageIp.getText()))
+                System.out.println(ageIp.getText());
+        });
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(nameIp,button);
+        layout.getChildren().addAll(ageIp,button);
 
         Scene scene = new Scene(layout, 300, 300);
         window.setScene(scene);
         window.show();
+    }
+
+    private boolean isInt(TextField nameIp, String text) {
+        try{
+            System.out.println("User is "+ Integer.parseInt(text));
+            return true;
+        }catch (NumberFormatException e){
+            System.out.println("Error: "+text+" is not a number");
+            return false;
+        }
     }
 }
