59311af4b02f81288f804be76173f186d84faefe
Adeel <71775487+ade3l@users.noreply.github.com>
2022-03-31 00:53:08 +0530

    Code for combo box added

--- src/main/java/com/example/javafxdemo/AK_Combo_box.java
+++ src/main/java/com/example/javafxdemo/AK_Combo_box.java
@@ -2,11 +2,14 @@ package com.example.javafxdemo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AK_Combo_box extends Application {
     Stage window;
+    ComboBox<String> comboBox;
     public static void main(String[] args) {
         launch(args);
     }
@@ -15,10 +18,27 @@ public class AK_Combo_box extends Application {
     public void start(Stage stage) throws Exception {
         window = stage;
 
+        comboBox = new ComboBox<>();
+        comboBox.getItems().addAll(
+                "Good will hunting",
+                "St. Vincent",
+                "Blackhat"
+        );
+        comboBox.setPromptText("Select favourite movie");
+        comboBox.setOnAction(e -> System.out.println("User selected: "+comboBox.getValue()));
+        //This will override the prompttext
+        comboBox.setEditable(true);
+        Button button = new Button("Click me");
+        button.setOnAction(e-> printMovie());
         VBox layout = new VBox(10);
-        layout.getChildren().addAll();
+        layout.getChildren().addAll(comboBox, button);
+
         Scene scene = new Scene(layout,300,300);
         window.setScene(scene);
         window.show();
+    }
+
+    private void printMovie() {
+        System.out.println(comboBox.getValue());
     }
 }
