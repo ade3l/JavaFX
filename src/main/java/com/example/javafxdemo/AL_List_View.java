1f5e0e633cceacd6e1f41a924d6ba4ec6c194c3e
Adeel <71775487+ade3l@users.noreply.github.com>
2022-04-02 03:00:12 +0530

    Code for listview added

--- src/main/java/com/example/javafxdemo/AL_List_View.java
+++ src/main/java/com/example/javafxdemo/AL_List_View.java
@@ -1,8 +1,11 @@
 package com.example.javafxdemo;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,11 +20,29 @@ public class AL_List_View extends Application {
         window = stage;
         Button button = new Button("Click me");
 
+        ListView<String> listView = new ListView<>();
+        listView.getItems().addAll("Iron man", "Titanic", "Contact", "Jodie Foster", "Surrogates");
+        /*By default you can only select only one item
+        You can change the selection model to change this*/
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
+        button.setOnAction(e-> getSelections(listView));
         VBox layout = new VBox(10);
-        layout.getChildren().addAll(button);
+        layout.getChildren().addAll(listView,button);
         Scene scene = new Scene(layout, 400,400);
         window.setScene(scene);
         window.show();
     }
+
+    private void getSelections(ListView listView) {
+        String message = "";
+        ObservableList<String> selections;
+        selections= listView.getSelectionModel().getSelectedItems();
+        for(String s: selections){
+            message += s+", ";
+        }
+        System.out.println(message);
+    }
+
+
 }
