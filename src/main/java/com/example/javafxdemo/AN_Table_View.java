4eb9e6a509b426c65a7487239dfe4188c57649e9
Adeel <71775487+ade3l@users.noreply.github.com>
2022-04-05 13:52:39 +0530

    Table view starter code added

---
+++ src/main/java/com/example/javafxdemo/AN_Table_View.java
@@ -0,0 +1,27 @@
+package com.example.javafxdemo;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class AN_Table_View extends Application {
+    Stage window;
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        window = stage;
+        window.setTitle("Table View");
+
+
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll();
+        Scene scene = new Scene(layout,400, 400);
+        window.setScene(scene);
+        window.show();
+    }
+}
