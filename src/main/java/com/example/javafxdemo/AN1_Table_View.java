618c780ecd7b0e3c66521490e903a7b4b533dd7f
Adeel <71775487+ade3l@users.noreply.github.com>
2022-04-05 14:14:39 +0530

    Code for simple table view added

--- src/main/java/com/example/javafxdemo/AN1_Table_View.java
+++ src/main/java/com/example/javafxdemo/AN1_Table_View.java
@@ -1,12 +1,18 @@
 package com.example.javafxdemo;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AN1_Table_View extends Application {
     Stage window;
+    TableView<AN2_Product> table;
     public static void main(String[] args) {
         launch(args);
     }
@@ -16,12 +22,40 @@ public class AN1_Table_View extends Application {
         window = stage;
         window.setTitle("Table View");
 
+        //Name column
+        //Column name can be anything we want
+        TableColumn<AN2_Product, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+//        name of value in  Property value factory must be the same as in object
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
 
+        //Price column
+        TableColumn<AN2_Product, Double> priceColumn = new TableColumn<>("Price");
+        priceColumn.setMinWidth(200);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        TableColumn<AN2_Product, Double> quantityColumn = new TableColumn<>("quantity");
+        quantityColumn.setMinWidth(200);
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+
+        table = new TableView<>();
+        table.setItems(getProducts());
+        table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
 
         VBox layout = new VBox();
-        layout.getChildren().addAll();
-        Scene scene = new Scene(layout,400, 400);
+        layout.getChildren().addAll(table);
+        Scene scene = new Scene(layout,600, 400);
         window.setScene(scene);
         window.show();
+    }
+//Function to get all products
+    public ObservableList<AN2_Product> getProducts(){
+        ObservableList<AN2_Product> products = FXCollections.observableArrayList();
+        products.add(new AN2_Product("Laptop",860,15));
+        products.add(new AN2_Product("Phone",2400,30));
+        products.add(new AN2_Product("Ball",20,7));
+        products.add(new AN2_Product("Corn",3,10));
+        products.add(new AN2_Product("Charger",120,5));
+        return products;
     }
 }
