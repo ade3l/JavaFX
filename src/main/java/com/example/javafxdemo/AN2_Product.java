6adfffc4cbb60656a023709af9444f631c0ef01e
Adeel <71775487+ade3l@users.noreply.github.com>
2022-04-05 13:56:19 +0530

    Created Product class

---
+++ src/main/java/com/example/javafxdemo/AN2_Product.java
@@ -0,0 +1,43 @@
+package com.example.javafxdemo;
+
+public class AN2_Product {
+    String name;
+    double price;
+    int quantity;
+
+    public AN2_Product() {
+        name = "";
+        price = 0;
+        quantity = 0;
+    }
+
+    public AN2_Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
