79e10e0f118d9296f523272151dcf30b8ea908f2
Adeel <71775487+ade3l@users.noreply.github.com>
2022-04-07 15:57:20 +0530

    Added text fields and button front end

--- src/main/java/com/example/javafxdemo/AN3_Editable_tables.java
+++ src/main/java/com/example/javafxdemo/AN3_Editable_tables.java
@@ -3,17 +3,21 @@ package com.example.javafxdemo;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AN3_Editable_tables extends Application {
     Stage window;
     TableView table;
+    TextField nameInput, priceInput, quantityInput;
 
     public static void main(String[] args) {
         launch(args);
@@ -39,12 +43,33 @@ public class AN3_Editable_tables extends Application {
         quantityColumn.setMinWidth(200);
         quantityColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
 
+        nameInput = new TextField();
+        nameInput.setPromptText("Name");
+        nameInput.setMinWidth(100);
+
+        priceInput = new TextField();
+        priceInput.setPromptText("Price");
+        priceInput.setMinWidth(100);
+
+        quantityInput = new TextField();
+        quantityInput.setPromptText("Quantity");
+        quantityInput.setMinWidth(100);
+
+        Button addButton = new Button("Add");
+        Button delButton = new Button("Delete");
+
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(10,10,10,10));
+        //Add spacing between elements inside the hBox
+        hBox.setSpacing(10);
+        hBox.getChildren().addAll(nameInput, priceInput, quantityInput, addButton, delButton);
+
         table = new TableView<>();
         table.setItems(getProducts());
         table.getColumns().addAll(nameColumn, priceColumn, quantityColumn);
 
         VBox layout = new VBox();
-        layout.getChildren().addAll(table);
+        layout.getChildren().addAll(table,hBox);
         Scene scene = new Scene(layout,600, 400);
         window.setScene(scene);
         window.show();
