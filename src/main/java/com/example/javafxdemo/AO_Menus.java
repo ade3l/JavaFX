3753948603a1bff0948e31387b23893675193643
Adeel <71775487+ade3l@users.noreply.github.com>
2022-04-09 23:21:11 +0530

    Created simple menu frontend

--- src/main/java/com/example/javafxdemo/AO_Menus.java
+++ src/main/java/com/example/javafxdemo/AO_Menus.java
@@ -2,6 +2,9 @@ package com.example.javafxdemo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -15,9 +18,17 @@ public class AO_Menus extends Application {
     public void start(Stage stage){
         window = stage;
         window.setTitle("Menu demo");
+        //File menu
+        Menu fileMenu = new Menu("File");
+        fileMenu.getItems().add(new MenuItem("New project..."));
+        fileMenu.getItems().addAll(new MenuItem("New Module..."), new MenuItem("Import Project..."));
+
+        //Main menu
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(fileMenu);
 
         layout = new BorderPane();
-
+        layout.setTop(menuBar);
         Scene scene = new Scene(layout,400,400);
         window.setScene(scene);
         window.show();
